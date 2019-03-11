04c8ffe22f73c3142b49093d275eb00dc3e1d16b
Jesper Skoglund <jesper.skoglund@acobiaflux.se>
2019-03-11 13:46:26 +0100

    End of ep 1
    add fix for nullpointer exeption in mainactivity

---
+++ app/src/main/java/com/example/forecastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,9 @@
+package com.example.forecastmvvm.ui.settings
+
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : Fragment() {
+
+
+}

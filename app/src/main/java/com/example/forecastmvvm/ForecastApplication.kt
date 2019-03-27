9557254457becd626862f5eb52cf2c0485bd1d86
Jesper Skoglund <jesper.skoglund@acobiaflux.se>
2019-03-27 11:14:40 +0100

    Episode 5

---
+++ app/src/main/java/com/example/forecastmvvm/ForecastApplication.kt
@@ -0,0 +1,28 @@
+package com.example.forecastmvvm
+
+import android.app.Application
+import com.example.forecastmvvm.data.db.ForecastDatabase
+import com.example.forecastmvvm.data.network.*
+import com.example.forecastmvvm.data.repository.ForcastRepository
+import com.example.forecastmvvm.data.repository.ForcastRepositoryImpl
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
+
+class ForecastApplication : Application(), KodeinAware {
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@ForecastApplication))
+
+        bind() from singleton { ForecastDatabase(instance()) }
+        bind() from singleton { instance<ForecastDatabase>().currentWeatherDao() }
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind() from singleton { ApixuWeatherApiService(instance()) }
+        bind<WeatherNetworkDataSource>() with  singleton { WeatherNetworkDataSourceImpl(instance()) }
+        bind<ForcastRepository>() with  singleton { ForcastRepositoryImpl(instance(), instance()) }
+
+    }
+
+}

6c0131867fd6c3d0fb3a7a56fb61ccfb6722fdf7
niklascote <cten@student.chalmers.se>
2019-04-01 11:36:50 +0200

    error with emulator fixed apk's downloaded min 9,30 episode 3

--- app/src/main/java/com/example/forecastmvvm/ui/data/db/entity/Current.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/data/db/entity/Current.kt
@@ -1,33 +1,23 @@
 package com.example.forecastmvvm.ui.data.db.entity
 
+import androidx.room.Embedded
+import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName="current_Weather")
 data class CurrentWeatherEntry(
-    val cloud: Int,
     val condition: Condition,
     @SerializedName("feelslike_c")
     val feelslikeC: Double,
     @SerializedName("feelslike_f")
     val feelslikeF: Double,
-    @SerializedName("gust_kph")
-    val gustKph: Double,
-    @SerializedName("gust_mph")
-    val gustMph: Double,
-    val humidity: Int,
     @SerializedName("is_day")
     val isDay: Int,
-    @SerializedName("last_updated")
-    val lastUpdated: String,
-    @SerializedName("last_updated_epoch")
-    val lastUpdatedEpoch: Int,
+    @Embedded(prefix = "condition_")
     @SerializedName("precip_in")
     val precipIn: Double,
     @SerializedName("precip_mm")
     val precipMm: Double,
-    @SerializedName("pressure_in")
-    val pressureIn: Double,
-    @SerializedName("pressure_mb")
-    val pressureMb: Double,
     @SerializedName("temp_c")
     val tempC: Double,
     @SerializedName("temp_f")
@@ -37,8 +27,6 @@ data class CurrentWeatherEntry(
     val visKm: Double,
     @SerializedName("vis_miles")
     val visMiles: Double,
-    @SerializedName("wind_degree")
-    val windDegree: Int,
     @SerializedName("wind_dir")
     val windDir: String,
     @SerializedName("wind_kph")

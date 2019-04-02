934c7e214e145b6ab479654cd115e383dc7d9872
niklascote <cten@student.chalmers.se>
2019-04-02 16:42:28 +0200

    episode 10, min 30:00

--- app/src/main/java/com/example/forecastmvvm/ui/ForecastApplication.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/ForecastApplication.kt
@@ -1,6 +1,7 @@
 package com.example.forecastmvvm.ui
 
 import android.app.Application
+import android.content.Context
 import com.example.forecastmvvm.ui.data.db.ForecastDatabase
 import com.example.forecastmvvm.ui.data.network.*
 import com.example.forecastmvvm.ui.data.provider.LocationProvider
@@ -10,6 +11,7 @@ import com.example.forecastmvvm.ui.data.provider.UnitProviderImpl
 import com.example.forecastmvvm.ui.data.repository.ForecastRepository
 import com.example.forecastmvvm.ui.data.repository.ForecastRepositoryImpl
 import com.example.forecastmvvm.ui.ui.settings.weather.current.CurrentWeatherViewModelFactory
+import com.google.android.gms.location.LocationServices
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
@@ -30,7 +32,9 @@ class ForecastApplication: Application(), KodeinAware {
         bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
         bind() from singleton { ApixuWeatherApiService(instance()) }
         bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
-        bind<LocationProvider>() with singleton { LocationProviderImpl() }
+
+        bind() from provider { LocationServices.getFusedLocationProviderClient(instance<Context>()) }
+        bind<LocationProvider>() with singleton { LocationProviderImpl(instance(), instance()) }
         bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(),instance(),instance(),instance()) }
         bind<UnitProvider>() with singleton { UnitProviderImpl(instance()) }
         bind() from provider { CurrentWeatherViewModelFactory(instance(),instance()) }
--- app/src/main/java/com/example/forecastmvvm/ui/ui/settings/MainActivity.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/ui/settings/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.example.forecastmvvm.ui.ui.settings
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.forecastmvvm.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
 import kotlinx.android.synthetic.main.activity_main.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+private const val MY_PERMISSION_ACCESS_COARSE_LOCATION = 1
+
+class MainActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by closestKodein()
+
+    private val fusedLocationProviderClient: FusedLocationProviderClient by instance()
 
     private lateinit var navController: NavController
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(p0: LocationResult?) {
+            super.onLocationResult(p0)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +50,33 @@ class MainActivity : AppCompatActivity() {
 
         NavigationUI.setupActionBarWithNavController(this, navController)
 
+        requestLocationPermission()
 
         }
 
         override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(null, navController)
+    }
+
+    private fun requestLocationPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+            MY_PERMISSION_ACCESS_COARSE_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ){
+        if (requestCode == MY_PERMISSION_ACCESS_COARSE_LOCATION){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                bindLocationManager()
+            else
+                Toast.makeText(this,"Please, set location manually in settings like a normal person", Toast.LENGTH_LONG)
+                    .show()
+        }
     }
 }

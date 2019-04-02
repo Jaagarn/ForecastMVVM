cff066383f2db1f67bcb516a973081464698cdd9
niklascote <cten@student.chalmers.se>
2019-04-02 10:11:58 +0200

    Episode 6 finished, not tested

---
+++ app/src/main/java/com/example/forecastmvvm/ui/internal/UnitSystem.kt
@@ -0,0 +1,5 @@
+package com.example.forecastmvvm.ui.internal
+
+enum class UnitSystem {
+    METRIC,IMPERIAL
+}
--- app/src/main/java/com/example/forecastmvvm/ui/ui/settings/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/ui/settings/weather/current/CurrentWeatherViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.forecastmvvm.ui.ui.settings.weather.current
 
 import androidx.lifecycle.ViewModel;
+import com.example.forecastmvvm.ui.data.repository.ForecastRepository
+import com.example.forecastmvvm.ui.internal.UnitSystem
+import com.example.forecastmvvm.ui.internal.lazyDeferred
 
-class CurrentWeatherViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CurrentWeatherViewModel(
+    private val forecastRepository: ForecastRepository
+) : ViewModel(){
+    private val unitSystem = UnitSystem.METRIC//Get from settings later
+    val isMetric: Boolean
+        get() = unitSystem == UnitSystem.METRIC
+
+    val weather by lazyDeferred() {
+        forecastRepository.getCurrentWeather(isMetric)
+    }
 }

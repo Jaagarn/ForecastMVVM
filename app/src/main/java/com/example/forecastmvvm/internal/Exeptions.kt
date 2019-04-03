47b87cc7073808e376203be03f61be5f7f03d888
Jesper Skoglund <jesper.skoglund@acobiaflux.se>
2019-04-03 08:50:51 +0200

    Ep 14 klar, allt funkar

--- app/src/main/java/com/example/forecastmvvm/internal/Exeptions.kt
+++ app/src/main/java/com/example/forecastmvvm/internal/Exeptions.kt
@@ -1,7 +1,10 @@
 package com.example.forecastmvvm.internal
 
 import java.io.IOException
+import java.lang.Exception
 
 class NoConnectivityException: IOException()
 
 class LocationPermissionNotGrantedExcpetion: IOException()
+
+class DateNotFoundException: Exception()

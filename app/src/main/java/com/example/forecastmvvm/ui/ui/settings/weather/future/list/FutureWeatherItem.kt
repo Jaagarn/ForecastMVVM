1d0ee87523c2accfe682c5c7f1a2a5335fcb0d47
niklascote <cten@student.chalmers.se>
2019-04-03 22:07:59 +0200

    Episode 13 finished but isnt working not sure why

---
+++ app/src/main/java/com/example/forecastmvvm/ui/ui/settings/weather/future/list/FutureWeatherItem.kt
@@ -0,0 +1,45 @@
+package com.example.forecastmvvm.ui.ui.settings.weather.future.list
+
+
+import com.example.forecastmvvm.R
+import com.example.forecastmvvm.ui.data.db.unitlocalized.future.MetricSimpleFutureWeatherEntry
+import com.example.forecastmvvm.ui.data.db.unitlocalized.future.UnitSpecificSimpleFutureWeatherEntry
+import com.example.forecastmvvm.ui.internal.glide.GlideApp
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.item_future_weather.*
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.FormatStyle
+
+
+class FutureWeatherItem(
+    val weatherEntry: UnitSpecificSimpleFutureWeatherEntry
+) : Item() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.apply {
+            textView_condition.text = weatherEntry.conditionText
+            updateDate()
+            updateTemperature()
+            updateConditionImage()
+        }
+    }
+
+    override fun getLayout() = R.layout.item_future_weather
+
+    private fun ViewHolder.updateDate(){
+        val dtFomatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+        textView_date.text = weatherEntry.date.format(dtFomatter)
+    }
+
+    private fun ViewHolder.updateTemperature(){
+        val unitAbbreviation = if (weatherEntry is MetricSimpleFutureWeatherEntry) "°C"
+        else "°F"
+        textView_temperature.text="${weatherEntry.avgTemperature}$unitAbbreviation"
+    }
+    private fun ViewHolder.updateConditionImage(){
+        GlideApp.with(this.containerView)
+            .load("http:"+weatherEntry.conditionIconUrl)
+            .into(imageView_condition_icon)
+    }
+
+}
